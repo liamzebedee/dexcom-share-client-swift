e29933be70b635fd93caa7d399b7eb2aa929998d
Liam Zebedeee <liamzebedee@yahoo.com.au>
2020-07-21 15:31:06 +1000

    Update ShareClient.swift

--- ShareClient/ShareClient.swift
+++ ShareClient/ShareClient.swift
@@ -41,7 +41,7 @@ public enum KnownShareServers: String {
          https://github.com/dabear/NightscoutShareServer
     */
 
-    // case CUSTOM="https://yourusernameshareserver.herokuapp.com"
+    case CUSTOM="https://customshareserver.liamz.co"
 }
 
 // From the Dexcom Share iOS app, via @bewest and @shanselman:
